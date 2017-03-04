baccafa4fcc8e7bcdb1edabbcb240331b81bfd0a
hello2all <hellot2@hotmail.com>
2017-03-04 19:14:50 +0800

    Initial commit

---
+++ stn_model.py
@@ -0,0 +1,52 @@
+import numpy as np
+from keras.layers import (Activation, Convolution2D, Dense, Dropout, Flatten,
+                          MaxPooling2D)
+from keras.models import Sequential, model_from_json
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import np_utils
+
+from spatial_transformer import SpatialTransformer
+
+IMG_SIZE = 32
+NUM_CLASSES = 43
+input_shape = (IMG_SIZE, IMG_SIZE, 3)
+
+def locnet():
+    b = np.zeros((2, 3), dtype='float32')
+    b[0, 0] = 1
+    b[1, 1] = 1
+    W = np.zeros((50, 6), dtype='float32')
+    weights = [W, b.flatten()]
+    locnet = Sequential()
+    locnet.add(MaxPooling2D(pool_size=(2, 2), input_shape=input_shape))
+    locnet.add(Convolution2D(20, 5, 5))
+    locnet.add(MaxPooling2D(pool_size=(2, 2)))
+    locnet.add(Convolution2D(20, 5, 5))
+
+    locnet.add(Flatten())
+    locnet.add(Dense(50))
+    locnet.add(Activation('relu'))
+    locnet.add(Dense(6, weights=weights))
+    return locnet
+
+def stn_model():
+
+    model = Sequential()
+    model.add(SpatialTransformer(localization_net=locnet(),
+                                 output_size=(32, 32),
+                                 input_shape=input_shape))
+
+    model.add(Convolution2D(32, 5, 5, border_mode='same',
+                            activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Convolution2D(64, 5, 5, border_mode='same',
+                            activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(NUM_CLASSES, activation='softmax'))
+    return model
---
+++ train_keras.py
@@ -0,0 +1,47 @@
+import pickle
+
+import numpy as np
+import tensorflow as tf
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+from keras.optimizers import Adagrad
+
+from floyd import dot
+from stn_model import stn_model
+
+training_file = dot + '/input/train.p'
+testing_file = dot + '/input/test.p'
+validating_file = dot + '/input/valid.p'
+
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
+with open(validating_file, mode='rb') as f:
+    valid = pickle.load(f)
+with open(testing_file, mode='rb') as f:
+    test = pickle.load(f)
+
+X_train, y_train = train['features'], train['labels']
+X_valid, y_valid = valid['features'], valid['labels']
+X_test, y_test = test['features'], test['labels']
+
+print("Number of training examples =", X_train.shape[0])
+print("Number of validating examples =", X_valid.shape[0])
+print("Number of testing examples =", X_test.shape[0])
+print("Image data shape =", X_train[0].shape)
+print("Number of classes =", len(np.unique(y_train)))
+
+batch_size = 20
+nb_epoch = 10000
+model = stn_model()
+adagrad = Adagrad(lr=0.00032, epsilon=1e-08, decay=0.0)
+model.compile(loss='sparse_categorical_crossentropy',
+          optimizer=adagrad,
+          metrics=['accuracy'])
+model.fit(X_train, y_train,
+          batch_size=batch_size,
+          nb_epoch=nb_epoch,
+          validation_data=(X_valid, y_valid),
+          shuffle=True,)
+
+y_pred = model.predict_classes(X_test)
+acc = np.sum(y_pred==y_test)/np.size(y_pred)
+print("Test accuracy = {}".format(acc))
